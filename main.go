f836a8e1ac9a1a781cde74c845a10323b1a83da2
sammyette <38820196+TorchedSammy@users.noreply.github.com>
2021-05-29 19:16:50 -0400

    feat: handle osk and load paths from json config

--- main.go
+++ main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
-	"log"
+	"encoding/json"
+	"fmt"
 	"path/filepath"
-	"strings"
 	"os"
+	"os/signal"
 
 	"github.com/fsnotify/fsnotify"
 )
 
+type TenshoConfig struct {
+	OsuDir string `json:"osuDir"`
+	SourceDir string `json:"sourceDir"`
+}
+
 func main() {
+	conffile, _ := os.ReadFile(os.Getenv("HOME") + "/.config/tensho/tensho.json")
+	conf := TenshoConfig{
+		OsuDir: os.Getenv("HOME") + "/.local/share/osu-wine/OSU",
+		SourceDir: os.Getenv("HOME") + "/Downloads",
+	}
+	json.Unmarshal(conffile, &conf)
+
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Fprintln(os.Stderr, err)
 	}
 	defer watcher.Close()
 
@@ -24,25 +37,48 @@ func main() {
 				if !ok {
 					return
 				}
-				if event.Op&fsnotify.Chmod == fsnotify.Chmod && strings.HasSuffix(event.Name, ".osz") {
-					beatmapname := filepath.Base(event.Name)
-					dest := os.Getenv("HOME") + "/.local/share/osu-wine/OSU/Songs/" + beatmapname
-					log.Printf("Moving %s to osu! song directory to %s\n", event.Name, dest)
-					os.Rename(event.Name, dest)
+				if event.Op&fsnotify.Chmod == fsnotify.Chmod {
+						conf.Copy(event.Name)
 				}
 			case err, ok := <-watcher.Errors:
 				if !ok {
 					return
 				}
-				log.Println("error:", err)
+				fmt.Println("error:", err)
 			}
 		}
 	}()
 
-	err = watcher.Add(os.Getenv("HOME") + "/Downloads")
+	err = watcher.Add(conf.SourceDir)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Fprintln(os.Stderr, err)
 	}
+	go handlesig()
 	<-done
 }
 
+func (c *TenshoConfig) Copy(filename string) {
+	dir := ""
+	switch filename[len(filename) - 4:] {
+	case ".osz":
+		dir = filepath.Join(c.OsuDir, "Songs")
+	case ".osk":
+		dir = filepath.Join(c.OsuDir, "Skins")
+	default:
+		return
+	}
+
+	beatmapname := filepath.Base(filename)
+	dest := filepath.Join(dir, beatmapname)
+	fmt.Printf("Moving %s to osu! song directory to %s\n", filename, dest)
+	os.Rename(filename, dest)
+}
+
+func handlesig() {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt)
+
+	for range c {
+		os.Exit(0)
+	}
+}
